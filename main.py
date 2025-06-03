310a19382ce67eb22f0fe597d21c3d743322b519
Daksh Kumar <dkumar2@sfsu.edu>
2025-06-02 19:58:28 -0700

    add usr input full name

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+# Ask for first name
+first_name = input("Enter your first name: ")
+
+# Ask for last name
+last_name = input("Enter your last name: ")
+
+# Combine and print full name
+full_name = first_name + " " + last_name
+print("Your full name is:", full_name)
