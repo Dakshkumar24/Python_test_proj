6b2536f4fc6c2bb7407153a41fe0a041274f5647
Daksh Kumar <dkumar2@sfsu.edu>
2025-06-02 20:03:01 -0700

    add code output

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ last_name = input("Enter your last name: ")
 # Combine and print full name
 full_name = first_name + " " + last_name
 print("Your full name is:", full_name)
+# # output
+# Enter your first name: Daksh
+# Enter your last name: Langhani
+# Your full name is: Daksh Langhani
